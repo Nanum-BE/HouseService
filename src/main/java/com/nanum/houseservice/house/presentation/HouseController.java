d78c2ab659e46e03eefe787eb15ff104a47c1de4
안대현 <gch05610@gmail.com>
2022-09-30 12:11:37 +0900

    [#11] feat: API 버전 관리 추가

--- src/main/java/com/nanum/houseservice/house/presentation/HouseController.java
+++ src/main/java/com/nanum/houseservice/house/presentation/HouseController.java
@@ -38,9 +38,8 @@ public class HouseController {
     private final HouseService houseService;
 
     @Operation(summary = "하우스 등록 API", description = "호스트가 하우스를 등록하는 요청")
-    @PostMapping("/houses/{hostId}")
-    public ResponseEntity<Object> createHouse(@PathVariable("hostId") Long hostId,
-                                              @Valid @RequestPart HouseRequest houseRequest,
+    @PostMapping("/v1/houses")
+    public ResponseEntity<Object> createHouse(@Valid @RequestPart HouseRequest houseRequest,
                                               @RequestPart(value = "houseMainImg", required = false) MultipartFile houseMainImg,
                                               @RequestPart(value = "floorPlanImg", required = false) MultipartFile floorPlanImg,
                                               @RequestPart(value = "houseFile", required = false) MultipartFile houseFile,
@@ -49,7 +48,6 @@ public class HouseController {
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         HouseDto houseDto = mapper.map(houseRequest, HouseDto.class);
-        houseDto.setHostId(hostId);
 
         if(houseFile == null || houseFile.isEmpty()) {
             throw new NoHouseFileException(String.format("HouseFile Cannot Be Empty"));
@@ -63,7 +61,7 @@ public class HouseController {
     }
 
     @Operation(summary = "본인 하우스 목록 조회 API", description = "호스트가 본인 하우스 목록을 조회하는 요청")
-    @GetMapping("/houses/{hostId}")
+    @GetMapping("/v1/houses/{hostId}")
     public ResponseEntity<Object> retrieveHostAllHouses(@PathVariable("hostId") Long hostId) {
 
         List<HostHouseResponse> response = houseService.retrieveHostAllHouses(hostId);
@@ -72,7 +70,7 @@ public class HouseController {
     }
 
     @Operation(summary = "본인 하우스 상세 조회 API", description = "호스트가 본인 하우스 상세 정보를 조회하는 요청")
-    @GetMapping("/houses/{hostId}/{houseId}")
+    @GetMapping("/v1/houses/{hostId}/{houseId}")
     public ResponseEntity<Object> retrieveHostHouse(@PathVariable("hostId") Long hostId,
                                                     @PathVariable("houseId") Long houseId) {
 
@@ -82,7 +80,7 @@ public class HouseController {
     }
 
     @Operation(summary = "하우스 수정 API", description = "호스트가 본인 하우스 정보 및 하우스 옵션을 수정하는 요청")
-    @PutMapping("/houses/{hostId}/{houseId}")
+    @PutMapping("/v1/houses/{hostId}/{houseId}")
     public ResponseEntity<Object> updateHouse(@PathVariable("hostId") Long hostId,
                                               @PathVariable("houseId") Long houseId,
                                               @Valid @RequestPart HouseUpdateRequest houseUpdateRequest,
@@ -102,7 +100,7 @@ public class HouseController {
     }
 
     @Operation(summary = "하우스 이미지 수정 API", description = "호스트가 본인 하우스 상세 이미지를 수정하는 요청")
-    @PutMapping("/houses/{hostId}/{houseId}/image")
+    @PutMapping("/v1/houses/{hostId}/{houseId}/image")
     public ResponseEntity<Object> updateHouseImg(@PathVariable("hostId") Long hostId,
                                               @PathVariable("houseId") Long houseId,
                                               @RequestPart(required = false) List<Long> deleteHouseImgs,
@@ -115,7 +113,7 @@ public class HouseController {
     }
 
     @Operation(summary = "하우스 서류 수정 API", description = "호스트가 본인 하우스 서류를 수정하는 요청")
-    @PutMapping("/houses/{hostId}/{houseId}/file")
+    @PutMapping("/v1/houses/{hostId}/{houseId}/file")
     public ResponseEntity<Object> updateHouseFile(@PathVariable("hostId") Long hostId,
                                                  @PathVariable("houseId") Long houseId,
                                                  @RequestPart(required = false) MultipartFile houseFile) {
@@ -131,7 +129,7 @@ public class HouseController {
     }
 
     @Operation(summary = "하우스 서류 조회 API", description = "하우스 서류를 조회하는 요청")
-    @GetMapping("/houses/{hostId}/{houseId}/file")
+    @GetMapping("/v1/houses/{hostId}/{houseId}/file")
     public ResponseEntity<Object> retrieveHouseFile(@PathVariable Long hostId,
                                                     @PathVariable Long houseId) {
 
