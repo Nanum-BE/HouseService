60872b9869f7b288dd4c38e56e959b461bbe81d1
mins00oo <alstn7223@gmail.com>
2022-10-19 08:07:08 +0900

    [#82] fix: kafka port 9092로 맞추게 수정/ 주석 필요없는 부분 처리

--- src/main/java/com/nanum/houseservice/kafka/messagequeue/KafkaConsumerConfig.java
+++ src/main/java/com/nanum/houseservice/kafka/messagequeue/KafkaConsumerConfig.java
@@ -21,7 +21,7 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> properties = new HashMap<>();
         // kafka 서버의 host, port, 컨슈머는 데이터를 받아오기 때문에 역직렬화 설정
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.18.0.101:9005");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.18.0.101:9092");
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
